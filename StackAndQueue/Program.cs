514fef54091cddcaca300e5a110bea99d6484e1a
Diwakar <diwakarn1599@gmail.com>
2021-07-10 22:07:28 +0530

    [Diwakar] Push data into stack

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -6,7 +6,13 @@ namespace StackAndQueue
     {
         static void Main(string[] args)
         {
+            StackMethods stack = new StackMethods();
             Console.WriteLine("Stack And Queue");
+
+            stack.Push(50);
+            stack.Push(76);
+            stack.Push(30);
+            stack.Display();
         }
     }
 }
---
+++ StackAndQueue/StackMethods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQueue
+{
+    class StackMethods
+    {
+        Node top;
+        public void Push(int data)
+        {
+            Node temp = new Node(data);
+            if(this.top == null)
+            {
+                temp.next = null;
+            }
+            else
+            {
+                temp.next = this.top;
+            }
+            this.top = temp;
+        }
+
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine($"|__{temp.data}__|");
+                temp = temp.next;
+            }
+        }
+    }
+}
